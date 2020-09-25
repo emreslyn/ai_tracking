cfd22ffa5e25929edc2c3b771599829cec52d251
Emre İşleyen 150160034 <56431108+emreslyn@users.noreply.github.com>
2020-09-25 14:31:58 +0300

    Update script.py
    sysInfo attribute is added.

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
+from systemInfo import SystemInfo
 class Script:
     file_name = ""
     server = ""
     condition = ""
+    sysInfo = ""
     def __init__(self,file_name,server,condition):
         self.file_name = file_name
         self.server = server
